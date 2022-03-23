75b98c083e0c5c45d876bee9fd8ed86e82f81525
Gillian Wang <gillian.yi.wang@gmail.com>
2022-03-23 17:34:55 -0400

    transactions.py updates

--- transactions.py
+++ transactions.py
@@ -1,8 +1,8 @@
 import sqlite3
 
 def to_tra_dict(tra_tuple):
-    ''' tra is a transation tuple (item #, amount, cat, date, desc)'''
-    tra = {'item #':tra_tuple[0], 'amount':tra_tuple[1], 'cat':tra_tuple[2], 'date':tra_tuple[3], 'desc': tra_tuple[4]}
+    ''' tra is a transation tuple (item #, amount, category, date, desc)'''
+    tra = {'item #':tra_tuple[0], 'amount':tra_tuple[1], 'category':tra_tuple[2], 'date':tra_tuple[3], 'desc': tra_tuple[4]}
     return tra
 
 def to_tra_dict_list(tra_tuples):
@@ -10,13 +10,13 @@ def to_tra_dict_list(tra_tuples):
     return [to_tra_dict(tra) for tra in tra_tuples]
 
 class Transaction():
-    ''' Category represents a table of transactions'''
+    ''' Transaction represents a table of transactions'''
 
     def __init__(self,dbfile):
         con= sqlite3.connect(dbfile)
         cur = con.cursor()
         cur.execute('''CREATE TABLE IF NOT EXISTS transactions
-                    (amount text, cat text, date text, desc text)''')
+                    (amount text, category text, date text, desc text)''')
         con.commit()
         con.close()
         self.dbfile = dbfile
@@ -32,10 +32,10 @@ class Transaction():
         return to_tra_dict_list(tuples)
 
     def select_one(self,itemnum):
-        ''' return a category with a specified itemnum '''
+        ''' return a transaction with a specified itemnum '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute("SELECT 'item #',* from categories where 'item #'=(?)",(itemnum,) )
+        cur.execute("SELECT 'item #',* from transactions where 'item #'=(?)",(itemnum,) )
         tuples = cur.fetchall()
         con.commit()
         con.close()
@@ -43,34 +43,34 @@ class Transaction():
 
 
     def add(self,transact):
-        ''' add a category to the categories table.
+        ''' add a transaction to the transactions table.
             this returns the rowid of the inserted element
         '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute("INSERT INTO transations VALUES(?,?)",(transact['amount'],transact['cat'],transact['date'],transact['desc']))
+        cur.execute("INSERT INTO transactions VALUES(?,?,?,?)",(transact['amount'],transact['category'],transact['date'],transact['desc']))
         con.commit()
-        cur.execute("SELECT last_insert_itemnum)")
+        cur.execute("SELECT last_insert_rowid()")
         last_rowid = cur.fetchone()
         con.commit()
         con.close()
         return last_rowid[0]
 
     def update(self,itemnum,transact):
-        ''' add a category to the transactions table.
+        ''' update a transaction to the transactions table.
             this returns the itemnum of the inserted element
         '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''UPDATE transactions
-                        SET amount=(?), cat=(?), date=(?), desc=(?)
+                        SET amount=(?), category=(?), date=(?), desc=(?)
                         WHERE 'item #'=(?);
-        ''',(transact['amount'],transact['cat'],transact['date'],transact['desc'],itemnum))
+        ''',(transact['amount'],transact['category'],transact['date'],transact['desc'],itemnum))
         con.commit()
         con.close()
 
     def delete(self,itemnum):
-        ''' add a category to the transactions table.
+        ''' delete a transaction to the transactions table.
             this returns the itemnum of the inserted element
         '''
         con= sqlite3.connect(self.dbfile)
