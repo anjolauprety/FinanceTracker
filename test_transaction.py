b3fa83a7f1b485481657054a0bf122f94af7d190
anjolauprety <anjolauprety@brandeis.edu>
2022-03-23 23:47:41 -0400

    pytest for transaction

--- test_transaction.py
+++ test_transaction.py
@@ -3,7 +3,8 @@ test_categories runs unit and integration tests on the category module
 '''
 
 import pytest
-from category import Category, to_cat_dict
+
+from transactions import Transaction, to_tra_dict
 
 
 @pytest.fixture
@@ -15,19 +16,22 @@ def dbfile(tmpdir):
 @pytest.fixture
 def empty_db(dbfile):
     ''' create an empty database '''
-    db = Category(dbfile)
+    db = Transaction(dbfile)
     yield db
 
 
 @pytest.fixture
 def small_db(empty_db):
     ''' create a small database, and tear it down later'''
-    cat1 = {'name': 'food', 'desc': 'groceries and takeout'}
-    cat2 = {'name': 'car', 'desc': 'gas and repairs'}
-    cat3 = {'name': 'fun', 'desc': 'movies and dining out'}
-    id1 = empty_db.add(cat1)
-    id2 = empty_db.add(cat2)
-    id3 = empty_db.add(cat3)
+    tra1 = {'amount': '1200', 'category': 'home furniture',
+            'date': '03/02/2022', 'desc': 'couch and coffee table'}
+    tra2 = {'amount': '1400', 'category': 'electronics',
+            'date': '03/19/2022', 'desc': 'tv and sound speaker'}
+    tra3 = {'amount': '800', 'category': 'dining',
+            'date': '03/22/2022', 'desc': 'groceries'}
+    id1 = empty_db.add(tra1)
+    id2 = empty_db.add(tra2)
+    id3 = empty_db.add(tra3)
     yield empty_db
     empty_db.delete(id3)
     empty_db.delete(id2)
@@ -41,10 +45,12 @@ def med_db(small_db):
     # add 10 categories
     for i in range(10):
         s = str(i)
-        cat = {'name': 'name'+s,
+        tra = {'amount': 'amount '+s,
+               'category': 'category '+s,
+               'date': 'date '+s,
                'desc': 'description '+s,
                }
-        rowid = small_db.add(cat)
+        rowid = small_db.add(tra)
         rowids.append(rowid)
 
     yield small_db
@@ -55,50 +61,51 @@ def med_db(small_db):
 
 
 @pytest.mark.simple
-def test_to_cat_dict():
-    ''' teting the to_cat_dict function '''
-    a = to_cat_dict((7, 'testcat', 'testdesc'))
-    assert a['rowid'] == 7
-    assert a['name'] == 'testcat'
+def test_to_tra_dict():
+    ''' testing the to_cat_dict function '''
+    a = to_tra_dict(('testamount', 'testcategory', 'testdate', 'testdesc'))
+    assert a['amount'] == 'testamount'
+    assert a['category'] == 'testcategory'
+    assert a['date'] == 'testdate'
     assert a['desc'] == 'testdesc'
-    assert len(a.keys()) == 3
+    assert len(a.keys()) == 4
 
 
 @pytest.mark.add
 def test_add(med_db):
     ''' add a category to db, the select it, then delete it'''
 
-    cat0 = {'name': 'testing_add',
+    tra0 = {'amount': 'testing_add',
             'desc': 'see if it works',
             }
-    cats0 = med_db.select_all()
-    rowid = med_db.add(cat0)
-    cats1 = med_db.select_all()
-    assert len(cats1) == len(cats0) + 1
-    cat1 = med_db.select_one(rowid)
-    assert cat1['name'] == cat0['name']
-    assert cat1['desc'] == cat0['desc']
+    tras0 = med_db.select_all()
+    rowid = med_db.add(tra0)
+    tras1 = med_db.select_all()
+    assert len(tras1) == len(tras0) + 1
+    tra1 = med_db.select_one(rowid)
+    assert tra1['amount'] == tra0['amount']
+    assert tra1['category'] == tra0['category']
 
 
 @pytest.mark.delete
 def test_delete(med_db):
     ''' add a category to db, delete it, and see that the size changes'''
     # first we get the initial table
-    cats0 = med_db.select_all()
+    tras0 = med_db.select_all()
 
     # then we add this category to the table and get the new list of rows
-    cat0 = {'name': 'testing_add',
+    tra0 = {'amount': 'testing_add',
             'desc': 'see if it works',
             }
-    rowid = med_db.add(cat0)
-    cats1 = med_db.select_all()
+    rowid = med_db.add(tra0)
+    tras1 = med_db.select_all()
 
     # now we delete the category and again get the new list of rows
     med_db.delete(rowid)
-    cats2 = med_db.select_all()
+    tras2 = med_db.select_all()
 
-    assert len(cats0) == len(cats2)
-    assert len(cats2) == len(cats1)-1
+    assert len(tras0) == len(tras2)
+    assert len(tras2) == len(tras1)-1
 
 
 @pytest.mark.update
@@ -106,16 +113,16 @@ def test_update(med_db):
     ''' add a category to db, updates it, and see that it changes'''
 
     # then we add this category to the table and get the new list of rows
-    cat0 = {'name': 'testing_add',
+    tra0 = {'amount': 'testing_add',
             'desc': 'see if it works',
             }
-    rowid = med_db.add(cat0)
+    rowid = med_db.add(tra0)
 
     # now we upate the category
-    cat1 = {'name': 'new cat', 'desc': 'new desc'}
-    med_db.update(rowid, cat1)
+    tra1 = {'amount': 'new amount', 'desc': 'new desc'}
+    med_db.update(rowid, tra1)
 
-    # now we retrieve the category and check that it has changed
-    cat2 = med_db.select_one(rowid)
-    assert cat2['name'] == cat1['name']
-    assert cat2['desc'] == cat1['desc']
+    # now we retrieve the transaction and check that it has changed
+    tra2 = med_db.select_one(rowid)
+    assert tra2['amount'] == tra1['amount']
+    assert tra2['desc'] == tra1['desc']
